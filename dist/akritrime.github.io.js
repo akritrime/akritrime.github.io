f90055d824dcd1d3dece75ac40edebf1c817b910
akritrime <akritrime@gmail.com>
2018-07-09 11:56:05 +0530

    skeleton

--- dist/akritrime.github.io.js
+++ dist/akritrime.github.io.js
@@ -71,7 +71,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({7:[function(require,module,exports) {
+})({8:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -101,7 +101,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],5:[function(require,module,exports) {
+},{}],7:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,25 +132,25 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":7}],6:[function(require,module,exports) {
+},{"./bundle-url":8}],6:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":5}],4:[function(require,module,exports) {
+},{"_css_loader":7}],4:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":5}],2:[function(require,module,exports) {
+},{"_css_loader":7}],2:[function(require,module,exports) {
 'use strict';
 
 require('normalize.css');
 
 require('./index.scss');
-},{"normalize.css":6,"./index.scss":4}],8:[function(require,module,exports) {
+},{"normalize.css":6,"./index.scss":4}],20:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -172,7 +172,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '39589' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '36929' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -273,5 +273,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[8,2])
+},{}]},{},[20,2])
 //# sourceMappingURL=/dist/akritrime.github.io.map
